ecc68dcb4cbe997f7e5604084ae681580f61b1a8
prlabu <1thisisfornothing1@gmail.com>
2019-04-19 00:10:54 +0800

    Object structures initialized - Chat, Message, Participant.

--- chat_parse.py
+++ chat_parse.py
@@ -1,9 +1,59 @@
 
 
 
+class Chat:
+    """
+    docstring
+    """
+
+    def __init__(self, ID: str, meta_: str):
+        self.messages = []
+        self.participants = set()
+        self.ID = chatID
+        self.meta = meta_
+
+    def anonymize():
+        pass
+
+    def replace_word(string_to_replace: str, replace_with: str):
+        pass
+    
+    def find_all_links():
+        pass     
+
+    def find_all_phones():
+        pass   
+
+    def to_csv(filename: str):
+        pass
+    
+    def to_json(filename: str):
+        pass
 
 
 
+
+class Participant:
+    """
+    docstring
+    """
+
+    def __init__(self, name: str):
+        self.name_in_chat = name
+
+
+
+
+class Message:
+    """
+    docstring
+    """
+
+    def __init__(self, datetime: datetime, sender: participant, orig_text: str, ):
+        self.datetime = datetime
+        self.sender = sender
+        self.orig_text = orig_text
+        self.
 
 
 
